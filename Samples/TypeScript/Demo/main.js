f9d53ea3f8922551a34bdc80ee0060777fce7d24
pandyCat <phaiz.archiplum@gmail.com>
2024-12-31 18:43:56 +0900

    # AI娘フロントエンドコミットしてるが、live2d モデルデータsayoは自分で買え

---
+++ Samples/TypeScript/Demo/main.js
@@ -0,0 +1,53 @@
+// アプリケーション作成用のモジュールを読み込み
+const { app, BrowserWindow, ipcMain, nativeTheme } = require("electron");
+const path = require("path");
+
+console.log("Let's play electron")
+
+// メインウィンドウ
+let mainWindow;
+
+const createWindow = () => {
+  // メインウィンドウを作成します
+  mainWindow = new BrowserWindow({
+    width: 700,
+    height: 500,
+    webPreferences: {
+      // プリロードスクリプトは、レンダラープロセスが読み込まれる前に実行され、
+      // レンダラーのグローバル（window や document など）と Node.js 環境の両方にアクセスできます。
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // メインウィンドウに表示するURLを指定します
+  // （今回はmain.jsと同じディレクトリのindex.html）
+  mainWindow.loadFile("index.html");
+
+  nativeTheme.themeSource = 'dark'
+
+  // メインウィンドウが閉じられたときの処理
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+};
+
+//  初期化が完了した時の処理
+app.whenReady().then(() => {
+  createWindow();
+
+  // アプリケーションがアクティブになった時の処理(Macだと、Dockがクリックされた時）
+  app.on("activate", () => {
+    // メインウィンドウが消えている場合は再度メインウィンドウを作成する
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// 全てのウィンドウが閉じたときの処理
+app.on("window-all-closed", () => {
+  // macOSのとき以外はアプリケーションを終了させます
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
